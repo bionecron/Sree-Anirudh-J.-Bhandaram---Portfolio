b30445844fe06c5ede87fbd11f3f3321acdda41b
Sree Anirudh J. Bhandaram <bionecron@gmail.com>
2011-02-09 18:55:38 -0500

    adding a file and testing a sync

---
+++ skills.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <title> Portfolio - Skills </title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <link type="text/css" rel="stylesheet" href="css/main.css" />
+        <?php
+            function __autoload($name)
+            {
+                include_once $name . ".php";
+            }
+        ?>
+        <?php include "includes/scripts.php"; ?>
+    </head>
+    
+    <body>
+        <div class="container">
+		<div class="header">
+		    <div class="headerTitle">
+		        <h1> <a href="index.php">SREE ANIRUDH J BHANDARAM</a> </h1>
+		        <span> Portfolio of a Usability, User Experience &amp; Web development Professional </span>
+		    </div>
+		</div>
+            <div class="content">
+                <div class="itemRow">
+                    <div class="itemData">
+                        <div class="itemTitle"> Key skills </div>
+                    </div>
+		    <div class="skillsItemBox">
+			<strong>Usability Engineering</strong>
+			<ul>
+				<li>Heuristic Evaluations</li>
+			    <li>Creating personas</li>
+			    <li>Task analysis</li>
+			    <li>Creating prototypes</li>
+			    <li>Conducting usability tests</li>
+			    <li>Summarizing results</li>
+			</ul>
+                    </div>
+                    <div class="skillsItemBox">
+			<strong>Usability Testing</strong>
+			<ul>
+			    <li>Interacting with a client</li>
+			    <li>Recruiting participants</li>
+			    <li>Creating test plans </li>
+			    <li>Moderating test sessions  </li>
+			    <li>Analyzing data &amp; presenting results</li>
+			    <li>Creating usability test reports</li>
+			</ul>
+                    </div>
+                     <div class="skillsItemBox">
+			<strong>User-Centered Design Methods</strong>
+			<ul>
+			    <li>Conducting contextual inquiry</li>
+			    <li>Creating design scenarios</li>
+			    <li>Claims analysis</li>
+			    <li>Storyboarding</li>
+			    <li>Work Modelling</li>
+			    <li>Storyboarding</li>
+			</ul>
+                    </div>
+		     <div class="skillsItemBox">
+			<strong>Eye Tracking</strong>
+			<ul>
+			    <li>Using SMI/Tobii/ASL eye trackers</li>
+			    <li>Using eye tracker analysis software (GazeTrack, EyeNal &amp; more)</li>
+			    <li>Creating eye tracking experiment design</li>
+			    <li>Analyzing fixations &amp; gaze plots</li>
+			    <li>Analyzing data and presenting results</li>
+			</ul>
+                    </div>
+		     <div class="skillsItemBox">
+			<strong>Prototyping &amp; Wireframing</strong>
+			<div>low + hi fidelity prototyping and wireframing using:</div>
+			<ul>
+			    <li>Adobe Fireworks</li>
+			    <li>Adobe Flash + ActionScript</li>
+			    <li>Adobe Photoshop</li>
+			    <li>HTML + CSS</li>
+			</ul>
+                    </div>
+		     <div class="skillsItemBox">
+			<strong>Web Development</strong>
+			<div>Hand-coding websites using: </div>
+			<ul>
+			    <li>HTML, xHTML, HTML5*</li>
+			    <li>PHP</li>
+			    <li>CSS2, CSS3*</li>
+			    <li>XML, XSLT</li>
+			    <li>JavaScript</li>
+			    <li>jQuery</li>
+			    <li>WordPress</li>
+			</ul>
+            </div>
+        <p style="font-size:12px"> * - I am currently learning HTML5 & CSS3</p>
+		</div>
+		</div>
+	    <?php include "includes/footer.php"; ?>
+	</div>
+	</body>
+</html>
